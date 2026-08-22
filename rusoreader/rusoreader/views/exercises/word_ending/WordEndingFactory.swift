95219ca0eccd3de07efba17eb25fe5b6b8594292
dhav211 <dhav211@gmail.com>
2026-08-22 10:09:36 -0700

    simplifies functions for choosing word form and handles opitional checking

--- rusoreader/rusoreader/views/exercises/word_ending/WordEndingFactory.swift
+++ rusoreader/rusoreader/views/exercises/word_ending/WordEndingFactory.swift
@@ -9,71 +9,70 @@ class WordEndingFactory : ExerciseFactory {
         self.word = word
         self.attempts = 0
         self.wordService = wordService
-        self.wordForm = setWordForm()
+        self.wordForm = getRandomWordFormForExercise(for: word)
     }
     
-    func createExercise() -> any Exercise {
-        return WordEndingExerciseView(viewModel: WordEndingExerciseViewModel(word: word, wordForm: wordForm!, wordService: wordService))
+    func createExercise() -> (any Exercise)? {
+        guard let wordForm = wordForm else { return nil }
+        return WordEndingExerciseView(viewModel: WordEndingExerciseViewModel(word: word, wordForm: wordForm, wordService: wordService))
     }
-    
-    private func setWordForm() -> WordForm? {
-        let acceptedFormTypes = getAccetableWordForms()
+
+    private func getRandomWordFormForExercise(for word: Word) -> WordForm? {
+        var acceptedFormTypes: [Form] {
+            if word.type == .adjective {
+                return [
+                    .adjectiveFemaleAccusative,
+                    .adjectiveFemaleDative,
+                    .adjectiveFemaleGenitive,
+                    .adjectiveFemaleInstrumental,
+                    .adjectiveFemalePrepositional,
+                    .adjectiveMaleAccusative,
+                    .adjectiveMaleDative,
+                    .adjectiveMaleGenitive,
+                    .adjectiveMaleInstrumental,
+                    .adjectiveMalePrepositional,
+                    .adjectiveNeuterAccusative,
+                    .adjectiveNeuterDative,
+                    .adjectiveNeuterGenitive,
+                    .adjectiveNeuterInstrumental,
+                    .adjectiveNeuterPrepositional,
+                    .adjectivePluralAccusative,
+                    .adjectivePluralDative,
+                    .adjectivePluralGenitive,
+                    .adjectivePluralInstrumental,
+                    .adjectivePluralPrepositional,
+                ]
+            } else if word.type == .noun {
+                return [
+                    .nounSingularDative,
+                    .nounSingularGenitive,
+                    .nounSingularAccusative,
+                    .nounSingularInstrumental,
+                    .nounSingularPrepositional,
+                    .nounPluralDative,
+                    .nounPluralGenitive,
+                    .nounPluralAccusative,
+                    .nounPluralInstrumental,
+                    .nounPluralPrepositional
+                ]
+            } else if word.type == .verb {
+                return [
+                    .verbPastMale,
+                    .verbPastFemale,
+                    .verbPastNeuter,
+                    .verbPastPlural,
+                    .verbPresentFuturePluralFirst,
+                    .verbPresentFuturePluralSecond,
+                    .verbPresentFuturePluralThird,
+                    .verbPresentFutureSingularFirst,
+                    .verbPresentFutureSingularSecond,
+                    .verbPresentFutureSingularThird,
+                ]
+            } else {
+                return []
+            }
+        }
         guard let randomFormType = acceptedFormTypes.randomElement() else { return nil }
         return word.forms.filter { $0.form == randomFormType }.first
-    }
-    
-    private func getAccetableWordForms() -> [Form] {
-        if word.type == .adjective {
-            return [
-                .adjectiveFemaleAccusative,
-                .adjectiveFemaleDative,
-                .adjectiveFemaleGenitive,
-                .adjectiveFemaleInstrumental,
-                .adjectiveFemalePrepositional,
-                .adjectiveMaleAccusative,
-                .adjectiveMaleDative,
-                .adjectiveMaleGenitive,
-                .adjectiveMaleInstrumental,
-                .adjectiveMalePrepositional,
-                .adjectiveNeuterAccusative,
-                .adjectiveNeuterDative,
-                .adjectiveNeuterGenitive,
-                .adjectiveNeuterInstrumental,
-                .adjectiveNeuterPrepositional,
-                .adjectivePluralAccusative,
-                .adjectivePluralDative,
-                .adjectivePluralGenitive,
-                .adjectivePluralInstrumental,
-                .adjectivePluralPrepositional,
-            ]
-        } else if word.type == .noun {
-            return [
-                .nounSingularDative,
-                .nounSingularGenitive,
-                .nounSingularAccusative,
-                .nounSingularInstrumental,
-                .nounSingularPrepositional,
-                .nounPluralDative,
-                .nounPluralGenitive,
-                .nounPluralAccusative,
-                .nounPluralInstrumental,
-                .nounPluralPrepositional
-            ]
-        } else if word.type == .verb {
-            return [
-                .verbPastMale,
-                .verbPastFemale,
-                .verbPastNeuter,
-                .verbPastPlural,
-                .verbPresentFuturePluralFirst,
-                .verbPresentFuturePluralSecond,
-                .verbPresentFuturePluralThird,
-                .verbPresentFutureSingularFirst,
-                .verbPresentFutureSingularSecond,
-                .verbPresentFutureSingularThird,
-            ]
-        } else {
-            return []
-        }
     }
 }
